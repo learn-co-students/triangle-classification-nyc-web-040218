8205633929081adff373de29de3c7719bfc0dd9d
Michael Myung <michael.h.myung@gmail.com>
2018-04-09 13:53:13 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c, :kind
+
+  def initialize(a,b,c)
+    @a = a 
+    @b = b
+    @c = c
+    #matrix = Matrix[a,b,c],[b,c,a],[c,a,b]?
+  end
+  
+  def kind
+    if is_not_a_triangle?
+      begin
+        raise TriangleError
+      end
+
+    elsif a == b && b == c
+      kind = :equilateral
+    elsif a == b || b == c || c == a
+      kind = :isosceles
+    elsif a != b && b != c
+      kind = :scalene
+    end
+  end
+  
+  def is_not_a_triangle?
+    if a <= 0 || b <= 0 || c <= 0
+      true
+    elsif a + b <= c || b + c <= a || c + a <= b
+      true
+    else
+      false
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
